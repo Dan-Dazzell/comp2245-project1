108ab0c4e30820a81ea5794620471763e9ce8d10
Jaheim Dazzell <bennyjaheim@gmail.com>
2025-11-05 02:04:32 -0400

    Added JS files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,18 @@
-/* Add your JavaScript to this file */
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('form');
+  const emailInput = document.querySelector('#email');
+  const messageDiv = document.querySelector('.message');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const email = emailInput.value.trim();
+
+    if (email) {
+      messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+    } else {
+      messageDiv.textContent = 'Please enter a valid email address';
+    }
+
+    emailInput.value = '';
+  });
+});
